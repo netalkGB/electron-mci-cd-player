ed6398cf1b31aef584709c4b7eaa712bab9ce751
gb <13611993+netalkGB@users.noreply.github.com>
2024-07-14 01:59:19 +0900

    Added support for displaying the number of tracks

--- cd-player/src/preload/preload.js
+++ cd-player/src/preload/preload.js
@@ -1,5 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('myAPI', {
-  test: () => { return ipcRenderer.invoke('do-athing')}
+// contextBridge.exposeInMainWorld('myAPI', {
+//   test: () => { return ipcRenderer.invoke('do-athing')}
+// })
+
+contextBridge.exposeInMainWorld('mci', {
+  openCd: () => { return ipcRenderer.invoke('open-cd')},
+  getTrackCount: () => { return ipcRenderer.invoke('get-track-count')},
+  closeCd: () => { return ipcRenderer.invoke('close-cd')}
 })
--- cd-player/src/renderer/renderer.js
+++ cd-player/src/renderer/renderer.js
@@ -1,1 +1,10 @@
-window.myAPI.test()
+// window.myAPI.test()
+
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await window.mci.openCd()
+    window.document.querySelector("#track-count").innerText = await window.mci.getTrackCount()
+    await window.mci.closeCd()
+});
