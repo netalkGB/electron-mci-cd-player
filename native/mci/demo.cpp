cced6bce67cf585199259769982c410b86bbe67d
gb <13611993+netalkGB@users.noreply.github.com>
2024-07-14 00:28:32 +0900

    echoを追加した

--- native/mci/demo.cpp
+++ native/mci/demo.cpp
@@ -10,14 +10,36 @@ namespace demo {
   using v8::Object;
   using v8::String;
   using v8::Value;
+  using v8::MaybeLocal;
+  using v8::Context;
 
   void Method(const FunctionCallbackInfo<Value>& args) {
     Isolate* isolate = args.GetIsolate();
     args.GetReturnValue().Set(String::NewFromUtf8(isolate, "Hello, World!").ToLocalChecked());
   }
 
+  void Echo(const FunctionCallbackInfo<Value>& args) {
+    Isolate* isolate = args.GetIsolate();
+
+    if (args.Length() < 1) {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong number of arguments").ToLocalChecked()));
+      return;
+    }
+
+    if (!args[0]->IsString()) {
+      isolate->ThrowException(Exception::TypeError(String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+      return;
+    }
+
+    Local<Context> context = isolate->GetCurrentContext();
+    Local<String> input = args[0]->ToString(context).ToLocalChecked();
+    args.GetReturnValue().Set(input);
+  }
+
+
   void Initialize(Local<Object> exports) {
     NODE_SET_METHOD(exports, "hello", Method);
+    NODE_SET_METHOD(exports, "echo", Echo);
   }
 
   NODE_MODULE(NODE_GYP_MODULE_NAME, Initialize)
