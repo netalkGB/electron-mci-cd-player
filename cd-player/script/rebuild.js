fe94db0252b9695c15f5e33197132fb1b988465a
gb <13611993+netalkGB@users.noreply.github.com>
2024-07-14 23:59:27 +0900

    electron用にビルドしなおすスクリプトを追加

---
+++ cd-player/script/rebuild.js
@@ -0,0 +1,26 @@
+const spawn = require('cross-spawn');
+const path = require('path');
+
+const cmakeNativeModulePaths = [
+    '../node_modules/mci'
+]
+
+cmakeNativeModulePaths.map(nativeModulePath => {
+    const mciPath = path.join(__dirname, nativeModulePath);
+
+    const cmakePath = path.join(__dirname, '../node_modules/.bin/cmake-js');
+
+    const cmakeProcess = spawn(cmakePath, ['rebuild'], { cwd: mciPath, stdio: 'inherit' });
+
+    cmakeProcess.on('close', (code) => {
+        if (code !== 0) {
+            console.error(`cmake-js rebuild process exited with code ${code}`);
+        } else {
+            console.log('cmake-js rebuild process completed successfully.');
+        }
+    });
+
+    cmakeProcess.on('error', (err) => {
+        console.error('Failed to start subprocess.', err);
+    });
+})
