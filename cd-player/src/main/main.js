c1071f693f36f605575f84809ca4971f5552fc43
gb <13611993+netalkGB@users.noreply.github.com>
2024-07-22 02:40:18 +0900

    ボタンを追加

--- cd-player/src/main/main.js
+++ cd-player/src/main/main.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const mci = require('mci')
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 500,
+    height: 120,
     webPreferences: {
       preload: path.join(__dirname, '..', 'preload', 'preload.js'),
       contextIsolation: true,
@@ -37,6 +37,77 @@ const createWindow = () => {
     return new Promise((resolve, reject) => {
       try {
         resolve(mci.closeCd())
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('play', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.play(...args))
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('stop', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.stop())
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+
+  ipcMain.handle('get-current-position', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.getCurrentPosition())
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('get-track-length', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.getTrackLength(...args))
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('get-current-track-number', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.getCurrentTrackNumber())
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('pause', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.pause())
+      } catch(e) {
+        reject(e)
+      }
+    })
+  })
+
+  ipcMain.handle('resume', (event, ...args) => {
+    return new Promise((resolve, reject) => {
+      try {
+        resolve(mci.resume())
       } catch(e) {
         reject(e)
       }
--- cd-player/src/preload/preload.js
+++ cd-player/src/preload/preload.js
@@ -7,5 +7,12 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('mci', {
   openCd: () => { return ipcRenderer.invoke('open-cd')},
   getTrackCount: () => { return ipcRenderer.invoke('get-track-count')},
-  closeCd: () => { return ipcRenderer.invoke('close-cd')}
+  closeCd: () => { return ipcRenderer.invoke('close-cd')},
+  play: (track) => { return ipcRenderer.invoke('play', track)},
+  stop: () => { return ipcRenderer.invoke('stop')},
+  getCurrentPosition: () => { return ipcRenderer.invoke('get-current-position')},
+  getTrackLength: (track) => { return ipcRenderer.invoke('get-track-length', track)},
+  getCurrentTrackNumber: () => { return ipcRenderer.invoke('get-current-track-number')},
+  pause: () => { return ipcRenderer.invoke('pause')},
+  resume: () => { return ipcRenderer.invoke('resume')},
 })
