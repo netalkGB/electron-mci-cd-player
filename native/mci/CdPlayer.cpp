5d4359ccbb461b8831f39ffc9fbc5fb9bedd6334
gb <13611993+netalkGB@users.noreply.github.com>
2024-07-22 01:41:53 +0900

    再生、停止、一時停止、トラック番号取得、トラックの長さ、再生位置取得のメソッドを追加した

--- native/mci/CdPlayer.cpp
+++ native/mci/CdPlayer.cpp
@@ -31,6 +31,53 @@ namespace mci {
     return _CloseCd() != 0;
   }
 
+  void CdPlayer::Play(int track) {
+    Stop();
+    if (_Play(GetTrackStart(track))) {
+      CloseCd();
+      throw CdPlayerException("Error playing track");
+    }
+  }
+
+  void CdPlayer::Stop() {
+    _Stop();
+  }
+
+  void CdPlayer::Pause() {
+    _Pause();
+  }
+
+  void CdPlayer::Resume() {
+    _Resume();
+  }
+
+  int CdPlayer::GetCurrentTrackNumber() {
+    if (_GetCurrentTrackNumber()) {
+      CloseCd();
+      throw CdPlayerException("Error getting current track number");
+    }
+    return (int) mciStatusParms.dwReturn;
+  }
+
+  unsigned long CdPlayer::GetTrackLength(int track) {
+    if (_GetTrackLength(track)) {
+      CloseCd();
+      throw CdPlayerException("Error getting track length");
+    }
+    return ConvertToMilliseconds(mciStatusParms.dwReturn);
+  }
+
+  unsigned long CdPlayer::GetCurrentPosition() {
+    if (_GetCurrentPosition()) {
+      CloseCd();
+      throw CdPlayerException("Error getting current position");
+    }
+    DWORD dwCurrentPosition = mciStatusParms.dwReturn;
+    DWORD dwRelativePosition = dwCurrentPosition - GetTrackStart(GetCurrentTrackNumber());
+    return ConvertToMilliseconds(dwRelativePosition);
+  }
+
+
   DWORD CdPlayer::_OpenCd() {
     mciOpenParms.lpstrDeviceType = "cdaudio";
     mciOpenParms.lpstrElementName =  DRIVE_LETTER;
@@ -44,5 +91,59 @@ namespace mci {
 
   DWORD CdPlayer::_CloseCd() {
     return mciSendCommand(mciOpenParms.wDeviceID, MCI_CLOSE, 0, (DWORD_PTR) &mciGenericParms);
+  }
+
+  DWORD CdPlayer::_GetTrackStart(DWORD dwTrack) {
+    mciStatusParms.dwItem = MCI_STATUS_POSITION;
+    mciStatusParms.dwTrack = dwTrack;
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_STATUS, MCI_STATUS_ITEM | MCI_TRACK, (DWORD_PTR) &mciStatusParms);
+  }
+
+  DWORD CdPlayer::GetTrackStart(DWORD dwTrack) {
+    if (_GetTrackStart(dwTrack)) {
+      CloseCd();
+      throw CdPlayerException("Error getting track start");
+    }
+    return mciStatusParms.dwReturn;
+  }
+
+  DWORD CdPlayer::_Play(DWORD dwTrackStart) {
+    mciPlayParms.dwFrom = dwTrackStart;
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_PLAY, MCI_FROM, (DWORD_PTR) &mciPlayParms);
+  }
+
+  DWORD CdPlayer::_Stop() {
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_STOP, 0, (DWORD_PTR) &mciGenericParms);
+  }
+
+  DWORD CdPlayer::_Pause() {
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_PAUSE, 0, 0);
+  }
+
+  DWORD CdPlayer::_Resume() {
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_RESUME, 0, 0);
+  }
+
+  DWORD CdPlayer::_GetCurrentTrackNumber() {
+    mciStatusParms.dwItem = MCI_STATUS_CURRENT_TRACK;
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_STATUS, MCI_STATUS_ITEM, (DWORD_PTR) &mciStatusParms);
+  }
+
+  DWORD CdPlayer::_GetTrackLength(DWORD dwTrack) {
+    mciStatusParms.dwItem = MCI_STATUS_LENGTH;
+    mciStatusParms.dwTrack = dwTrack;
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_STATUS, MCI_STATUS_ITEM | MCI_TRACK, (DWORD_PTR) &mciStatusParms);
+  }
+
+  DWORD CdPlayer::ConvertToMilliseconds(DWORD dwTime) {
+    DWORD minutes = MCI_MSF_MINUTE(dwTime);
+    DWORD seconds = MCI_MSF_SECOND(dwTime);
+    DWORD frames = MCI_MSF_FRAME(dwTime);
+    return (minutes * 60000) + (seconds * 1000) + (frames * (1000 / 75));
+  }
+
+  DWORD CdPlayer::_GetCurrentPosition() {
+    mciStatusParms.dwItem = MCI_STATUS_POSITION;
+    return mciSendCommand(mciOpenParms.wDeviceID, MCI_STATUS, MCI_STATUS_ITEM, (DWORD_PTR) &mciStatusParms);
   }
 } // mci
