47186f7032c9729d8eb7f2e715946b0d7a30a8c6
gb <13611993+netalkGB@users.noreply.github.com>
2024-08-27 23:14:12 +0900

    Changed the namespace to mci

--- native/mci/CdPlayerException.h
+++ native/mci/CdPlayerException.h
@@ -5,16 +5,18 @@
 #include <exception>
 #include <string>
 
-class CdPlayerException : public std::exception {
-public:
-  CdPlayerException(const std::string& message) : message_(message) {}
+namespace mci {
+  class CdPlayerException : public std::exception {
+  public:
+    CdPlayerException(const std::string& message) : message_(message) {}
 
-  virtual const char* what() const noexcept override {
-    return message_.c_str();
-  }
+    virtual const char* what() const noexcept override {
+      return message_.c_str();
+    }
 
-private:
-  std::string message_;
-};
+  private:
+    std::string message_;
+  };
+}
 
 #endif // CDPLAYEREXCEPTION_H
