ad91ec896fcbaa02f2eb3c7865b50e74fe42396d
gb <13611993+netalkGB@users.noreply.github.com>
2025-07-27 04:20:03 +0900

    Enhance third-party licenses

--- cd-player/script/generate-license-report.mjs
+++ cd-player/script/generate-license-report.mjs
@@ -1,34 +1,65 @@
-import checker from 'license-checker'
+import * as checker from 'license-checker-rseidelsohn'
 import fs from 'fs/promises'
 import path from 'path'
 
 checker.init({
-  start: './'
+  start: './',
+  production: true,
+  excludePrivatePackages: true
 }, async function (err, packages) {
   if (err) {
     return
   }
   const keys = Object.keys(packages)
-  let report = ''
+  let report = 'Third-Party Software Licenses\n This software uses the following third-party packages:\n'
+  let licenseSet = new Set()
   for (const key of keys) {
+    report += '--------------------------------------\n'
     const { licenses, repository, publisher, url, licenseFile } = packages[key]
     report += `${key}\n`
     if (licenses) report += `License: ${licenses}\n`
     if (repository) report += `Repository: ${repository}\n`
     if (publisher) report += `Publisher: ${publisher}\n`
     if (url) report += `URL: ${url}\n`
-    if (licenseFile) {
+
+    const filePath = packages[key].path
+    if (licenses.includes(' OR')) {
+      const licenseFiles = await fs.readdir(filePath)
+      const licenseFilePaths = licenseFiles.filter(file => file.match(/LICENSE/i))
+      if (licenseFilePaths.length > 0) {
+        for (const licenseFilePath of licenseFilePaths) {
+          try {
+            const data = await fs.readFile(path.join(filePath, licenseFilePath), 'utf8')
+            report += `License:\n${data}\n`
+          } catch (err) {
+            console.error(`Error reading license file for ${key}:`, err)
+            process.exit(1)
+          }
+        }
+      } else {
+        console.error(`No license file found for ${key}`)
+        process.exit(1)
+      }
+    } else if (licenseFile) {
       const fileName = path.basename(licenseFile)
       if (fileName.match(/LICENSE/i)) {
         try {
           const data = await fs.readFile(licenseFile, 'utf8')
           report += `License:\n${data}\n`
         } catch (err) {
-          console.error(err)
+          console.error(`Error reading license file for ${key}:`, err)
+          process.exit(1)
         }
+      } else {
+        console.error(`No license file found for ${key}`)
+        process.exit(1)
       }
+    } else {
+      console.error(`No license file found for ${key}`)
+      process.exit(1)
     }
-    report += '\n--------------------------------------\n'
+    licenseSet.add(licenses)
   }
+  // console.log('Licenses:', licenseSet)
   await fs.writeFile(path.join('dist', 'license.txt'), report, 'utf8')
 })
